b919e83f025dada346d54d7e5b36d2d25802038a
BJJoiner <joiner_brandon@columbusstate.edu>
2021-08-29 23:14:37 +0000

    defined multiply and divide, add print statement multiply and divide, add if statement subtract, multiply and divide

--- calculator.py
+++ calculator.py
@@ -4,13 +4,17 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
-#def multiply
+def multiply(x, y):
+    return x * y
 
-#def divide
+def divide(x, y):
+    return x / y
 
 print("Select Operation.")
 print("1.Add")
 print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 
 
 while True:
@@ -21,6 +25,12 @@ while True:
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
         break
     else:
         print("Invalid Input")
